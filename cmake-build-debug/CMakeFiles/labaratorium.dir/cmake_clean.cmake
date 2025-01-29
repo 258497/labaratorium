2de2649c43529ca1c352aef00ef550848138f887
258497 <258497@edu.p.lodz.pl>
2025-01-29 02:04:58 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/labaratorium.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labaratorium.dir/main.cpp.o"
+  "CMakeFiles/labaratorium.dir/main.cpp.o.d"
+  "labaratorium"
+  "labaratorium.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labaratorium.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
